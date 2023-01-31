e19d29b4338dbcd8e05e752a40da134d46009db3
Ali Zoubair <88006585+alizoubair@users.noreply.github.com>
2023-01-31 21:29:58 +0100

    websocket server

---
+++ server/go.mod
@@ -0,0 +1,5 @@
+module github.com/alizoubair/wsproxy
+
+go 1.19
+
+require github.com/gorilla/websocket v1.5.0 // indirect
---
+++ server/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+    "log"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+    c, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        log.Printf("Upgrading error: %#v\n", err)
+        return
+    }
+    defer c.Close()
+
+    log.Println("Success to handshake with client")
+
+    for {
+        mt, message, err := c.ReadMessage()
+        if err != nil {
+            log.Printf("Reading error: %#v\n", err)
+            break
+        }
+        log.Printf("Received: message %q", message)
+        if err := c.WriteMessage(mt, message); err != nil {
+            log.Printf("Writing error: %#v\n", err)
+            break
+        }
+    }
+}
+
+func main() {
+	p := 8080
+	log.Printf("Starting websocket echo server on port %d", p)
+
+	http.HandleFunc("/", echo)
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", p), nil); err != nil {
+		log.Panicf("Error while starting to listen: %#v", err)
+	}
+}
